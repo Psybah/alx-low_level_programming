9d5c608f2f1b78f91f4aeb751b165d9e684511a8
Psybah <abiodunoluwamurewa@gmail.com>
2023-10-06 19:03:32 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,15 +10,15 @@ int main(void)
 
 	for (i = 0; i <= 7; i++)
 	{
-		for (j = i + 1; j <=8; j++)
+		for (j = i + 1; j <= 8; j++)
 		{
-			for (k = j + 1; k <=9; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				putchar(i + '0');
-				putchar(j + 'o');
+				putchar(j + '0');
 				putchar(k + '0');
 
-				if (i != 7 || j !=8 || k != 9)
+				if (!(i == 7 && j == 8 && k == 9))
 				{
 					putchar(',');
 					putchar(' ');
