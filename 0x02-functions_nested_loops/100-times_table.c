e14d0823dedd6a947d22b98d12a52a0363d360a1
Psybah <abiodunoluwamurewa@gmail.com>
2023-10-11 06:09:38 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,10 +6,10 @@
  */
 void print_times_table(int n)
 {
+	int i, j, result;
+
 	if (n < 0 || n > 15)
 		return;
-
-	int i, j, result;
 
 	for (i = 0; i <= n; i++)
 	{
@@ -17,27 +17,19 @@ void print_times_table(int n)
 		{
 			result = i * j;
 
-			if (j > 0)
-				_putchar(',');
-
-			if (result < 10)
+			if (j == 0)
 			{
-				if (j > 0)
-					_putchar(' ');
-
-				_putchar(' ');
-				_putchar(' ');
 				_putchar(result + '0');
-			}
-			else if (result < 100)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
 			}
 			else
 			{
+				_putchar(',');
+
+				if (result < 10)
+					_putchar(' ');
+				if (result < 100)
+					_putchar(' ');
+
 				_putchar(' ');
 				_putchar((result / 100) + '0');
 				_putchar(((result / 10) % 10) + '0');
@@ -47,4 +39,3 @@ void print_times_table(int n)
 		_putchar('\n');
 	}
 }
-
