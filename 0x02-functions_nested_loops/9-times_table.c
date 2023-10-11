7cc4ae6b6c33e1a8b6a0b37db038f76bf0133425
Psybah <abiodunoluwamurewa@gmail.com>
2023-10-10 18:07:02 -0700

    Updated Task 9 againgit add .

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,21 +12,21 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-			if (j != 0)
+
+			if (j == 0)
 			{
-				_putchar(';');
-				_putchar(' ');
-			}
-			if (result < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
+				_putchar(result + '0');
 			}
 			else
 			{
-				_putchar((result / 10) + '0');
+				_putchar(',');
+
+				if (result < 10)
+					_putchar(' ');
+
+				_putchar(' ');
+				_putchar((result % 10) + '0');
 			}
-			_putchar((result % 10) + '0');
 		}
 		_putchar('\n');
 	}
