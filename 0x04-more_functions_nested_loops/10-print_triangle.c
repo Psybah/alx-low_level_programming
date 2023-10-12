fb4450fc8e134f6a7a8c9e43638b3c4f6df424f1
Psybah <abiodunoluwamurewa@gmail.com>
2023-10-12 11:56:44 -0700

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,21 +6,25 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
-
 	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size - i - 1; j++)
-			_putchar(' ');
+	int i, j;
 
-		for (j = 0; j <= i; j++)
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size - i; j++)
+		{
+			_putchar(' ');
+		}
+
+		for (j = 1; j <= i; j++)
+		{
 			_putchar('#');
+		}
 
 		_putchar('\n');
 	}
