b83c0fd339f5b8b880bdc7aa1360d4427b8460e8
Psybah <abiodunoluwamurewa@gmail.com>
2023-10-16 09:18:26 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts2(str);
+
+	str = "Lrmismdlrstae,cnettraiicn lt e oeumdtmo niiutu aoee ooemgaaiu.U nma ii eim usnsrdeectto lac aoi iiu lqi xe omd osqa.Di ueiuedlri erhnei nvlpaevltes ilmdlr ufga ul aitr xetu itocea uiaa o riet uti up u fii eeutmli nmi s aou.";
+	puts2(str);
+
+	return (0);
+}
+
+/**
  * puts2 - Prints every other character of a string.
  * @str: Pointer to the string.
  */
@@ -11,7 +29,7 @@ void puts2(char *str)
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
-		i += 2;  /* Move to every other character */
+		i += 2;
 	}
 
 	_putchar('\n');
