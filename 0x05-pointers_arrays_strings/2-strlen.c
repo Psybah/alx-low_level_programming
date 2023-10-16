4cd0db04da356113b3b7fa750f852bd1e7b22d49
Psybah <abiodunoluwamurewa@gmail.com>
2023-10-16 08:45:32 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,17 +4,17 @@
  * _strlen - Returns the length of a string.
  * @s: Pointer to the string.
  *
- * Return: The length of the string.
+ * Return: Length of the string.
  */
 int _strlen(char *s)
 {
-	int length = 0;
-	
+	int len = 0;
+
 	while (*s != '\0')
 	{
-		length++;
+		len++;
 		s++;
 	}
-	
-	return length;
+
+	return (len);
 }
