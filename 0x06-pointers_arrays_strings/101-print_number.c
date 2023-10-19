c35b6523ddd5267e7f52959f94e8d90e928f34c0
Psybah <abiodunoluwamurewa@gmail.com>
2023-10-19 07:05:58 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,14 +6,19 @@
  */
 void print_number(int n)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n == INT_MIN)
+		{
+			_putchar('2');
+			n = 147483648;
+		}
+		else
+		{
+			n = -n;
+		}
+	}
 
-    if (n / 10 != 0)
-        print_number(n / 10);
-
-    _putchar(n % 10 + '0');
+	if (n / 10 != 0)
 }
