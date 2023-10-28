b02b2bdd03334d343d0b2609e3a6f17b65227bf6
Psybah <abiodunoluwamurewa@gmail.com>
2023-10-27 19:59:58 -0700

    TAsk 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,59 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h> /* for atoi function */
+#include <stdlib.h>
 
 /**
- * count_coins - calculates the number of coins needed for change
- * @cents: the amount of cents
- * Return: the number of coins
- */
-int count_coins(int cents)
-{
-	int coins = 0;
-
-	while (cents >= 25)
-	{
-		cents -= 25;
-		coins++;
-	}
-	while (cents >= 10)
-	{
-		cents -= 10;
-		coins++;
-	}
-	while (cents >= 5)
-	{
-		cents -= 5;
-		coins++;
-	}
-	while (cents >= 2)
-	{
-		cents -= 2;
-		coins++;
-	}
-	while (cents >= 1)
-	{
-		cents -= 1;
-		coins++;
-	}
-
-	return (coins);
-}
-
-/**
- * main - calculates the minimum number of coins for change
- * @argc: argument count
- * @argv: argument vector
+ * main - Entry point of the program
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
+	int cents, i, count;
+	int coins[] = {25, 10, 5, 2, 1};
+	int num_coins = sizeof(coins) / sizeof(coins[0]);
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
 	if (cents < 0)
 	{
@@ -61,8 +27,17 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	int coins = count_coins(cents);
+	count = 0;
 
-	printf("%d\n", coins);
+	for (i = 0; i < num_coins; i++)
+	{
+		while (cents >= coins[i])
+		{
+			cents -= coins[i];
+			count++;
+		}
+	}
+
+	printf("%d\n", count);
 	return (0);
 }
