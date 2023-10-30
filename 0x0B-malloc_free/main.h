db3165c7627e8cfa32e219d530c95217e9561261
Psybah <abiodunoluwamurewa@gmail.com>
2023-10-30 09:35:51 -0700

    main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -11,7 +9,5 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
-static int count_words(char *str);
-static char *extract_word(char *str, int start, int end);
 
 #endif
