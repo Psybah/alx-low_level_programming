6168f53e9861ed2420ed48e0eac08131383920fd
Psybah <abiodunoluwamurewa@gmail.com>
2023-10-30 08:04:24 -0700

    Task 6

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -9,7 +11,7 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
-int count_words(char *str);
-char *extract_word(char *str, int start, int end);
+static int count_words(char *str);
+static char *extract_word(char *str, int start, int end);
 
 #endif
