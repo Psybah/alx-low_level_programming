69fa147060f6e5a2b7dfe7989223a775cfba4ee5
Psybah <abiodunoluwamurewa@gmail.com>
2023-11-08 03:32:13 -0800

    Updated 0-print_name.c, 1-array_iterator.c, 2-int_index.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -8,6 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name && f)
 		f(name);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,13 +10,17 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array != NULL && cmp != NULL && size > 0)
+	int i;
+
+	if (array && cmp)
 	{
-		for (int i = 0; i < size; i++)
-		{
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
 			if (cmp(array[i]))
 				return (i);
-		}
 	}
+
 	return (-1);
 }
