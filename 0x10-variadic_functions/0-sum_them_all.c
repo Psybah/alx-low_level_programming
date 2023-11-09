9d1a25e96072cb20220c856221ee071d51887512
Psybah <abiodunoluwamurewa@gmail.com>
2023-11-09 01:57:50 -0800

    Fix Betty style issues in 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,18 +9,19 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	int sum;
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+
 	if (n == 0)
 		return (0);
 
-	int sum = 0;
-	va_list args;
-	va_start(args, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
 
-	for (unsigned int i = 0; i < n; ++i) {
-		sum += va_arg(args, int);
-	}
-
-	va_end(args);
+	va_end(ap);
 
 	return (sum);
 }
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - Prints numbers, followed by a new line.
+ * @separator: String to be printed between numbers.
+ * @n: Number of integers passed to the function.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
